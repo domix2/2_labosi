402fa991d0d90880d2a2e9d99e4512757cc4aee3
Domagoj <domagoj.speljko@gmail.com>
2023-03-21 01:13:41 +0100

    Prvi dio 2 vježbi

---
+++ main.py
@@ -0,0 +1,11 @@
+kolegij ={}
+
+ime_kolegija = input('Unesite ime kolegija:').upper()
+broj_bodova = int(input('Unesite broj ECTS bodova:'))
+
+kolegij[ime_kolegija]=broj_bodova
+print("Kolegij", ime_kolegija ,'nosi',broj_bodova, 'ECTS bodova')
+
+
+
+
