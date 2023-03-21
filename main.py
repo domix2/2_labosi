6b380a3885dc8289979de4e8efe5e2f3eac3fe23
Domagoj <domagoj.speljko@gmail.com>
2023-03-21 12:41:21 +0100

    Završni kod sa 3 zadatkom riješenim

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import date
 
 kolegij ={}
 ispit = {}
-
+student = {}
 ime_kolegija = input("Unesite ime kolegija:")
 ects_bodovi = int(input("Unesite brioj ECTS bodova:"))
 kolegij[ime_kolegija.upper()] = ects_bodovi
@@ -15,8 +15,14 @@ datum_ispita = date(godina, mjesec, dan)
 ispit["kolegij"] = kolegij
 ispit ["datum"] = datum_ispita
 
+ime = input("Unesite ime studenta:").capitalize()
+prezime = input("Unesite prezime studenta:").capitalize()
+student["ispit"] =ispit
+student["ime"] = ime
+student["prezime"] = prezime
 print(" Kolegij",ime_kolegija,"nosi",ects_bodovi,"bodova")
 print("Datum ispita je:", datum_ispita)
 
+print("Student",ime,prezime," je prijavio ispit iz kolegija",ime_kolegija,"koji će se održati:",datum_ispita)
 
 
