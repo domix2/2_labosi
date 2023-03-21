2a69c0c752117d988292981ba4deecfa33aa4ddf
Domagoj <domagoj.speljko@gmail.com>
2023-03-21 12:24:40 +0100

    "Prva verzija koda za 2 vježbe"

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+from datetime import date
+
 kolegij ={}
+ispit = {}
 
-ime_kolegija = input('Unesite ime kolegija:').upper()
-broj_bodova = int(input('Unesite broj ECTS bodova:'))
+ime_kolegija = input("Unesite ime kolegija:")
+ects_bodovi = int(input("Unesite brioj ECTS bodova:"))
+kolegij[ime_kolegija.upper()] = ects_bodovi
 
-kolegij[ime_kolegija]=broj_bodova
-print("Kolegij", ime_kolegija ,'nosi',broj_bodova, 'ECTS bodova')
+godina = int(input("Unesite godinu ispita:"))
+mjesec = int(input("Unesite mjesec ispita:"))
+dan = int(input("Unesite dan ispita:"))
+datum_ispita = date(godina, mjesec, dan)
+
+ispit["kolegij"] = kolegij
+ispit ["datum"] = datum_ispita
+
+print(" Kolegij",ime_kolegija,"nosi",ects_bodovi,"bodova")
+print("Datum ispita je:", datum_ispita)
 
 
 
